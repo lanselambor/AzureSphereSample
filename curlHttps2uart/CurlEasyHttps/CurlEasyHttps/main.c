ac88d71d233dfd9fff579313796180d72aa1d810
lanselambor <757016515@qq.com>
2019-02-27 21:13:06 +0800

    add sample for curlHttps2uart

---
+++ curlHttps2uart/CurlEasyHttps/CurlEasyHttps/main.c
@@ -0,0 +1,349 @@
+﻿/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+#include <errno.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <curl/curl.h>
+
+// applibs_versions.h defines the API struct versions to use for applibs APIs.
+#include "applibs_versions.h"
+#include <applibs/log.h>
+#include <applibs/networking.h>
+#include <applibs/storage.h>
+#include <applibs/uart.h>
+#include <applibs/gpio.h>
+
+#include "mt3620_rdb.h"
+#include "epoll_timerfd_utilities.h"
+
+
+#define UART_LOG(str) SendUartMessage(uartFd, str)
+
+// This sample C application for Azure Sphere periodically downloads and outputs the index web page
+// at example.com, by using cURL over a secure HTTPS connection.
+// It uses the cURL 'easy' API which is a synchronous (blocking) API.
+//
+// It uses the following Azure Sphere libraries:
+// - log (messages shown in Visual Studio's Device Output window during debugging);
+// - storage (device storage interaction);
+// - curl (URL transfer library).
+
+static volatile sig_atomic_t terminationRequired = false;
+
+static int gpioLedFd5 = -1;
+static int gpioLedFd6 = -1;
+
+/// <summary>
+///     Signal handler for termination requests. This handler must be async-signal-safe.
+/// </summary>
+static void TerminationHandler(int signalNumber)
+{
+    // Don't use Log_Debug here, as it is not guaranteed to be async-signal-safe.
+    terminationRequired = true;
+}
+
+// Epoll and event handler file descriptors.
+static int webpageDownloadTimerFd = -1;
+static int epollFd = -1;
+static int uartFd = -1;
+
+/// <summary>
+///     Data pointer and size of a block of memory allocated on the heap.
+/// </summary>
+typedef struct {
+    char *data;
+    size_t size;
+} MemoryBlock;
+
+/// <summary>
+///     Helper function to send a fixed message via the given UART.
+/// </summary>
+/// <param name="uartFd">The open file descriptor of the UART to write to</param>
+/// <param name="dataToSend">The data to send over the UART</param>
+static void SendUartMessage(int uartFd, const char *dataToSend)
+{
+	size_t totalBytesSent = 0;
+	size_t totalBytesToSend = strlen(dataToSend);
+	int sendIterations = 0;
+	while (totalBytesSent < totalBytesToSend) {
+		sendIterations++;
+
+		// Send as much of the remaining data as possible
+		size_t bytesLeftToSend = totalBytesToSend - totalBytesSent;
+		const char *remainingMessageToSend = dataToSend + totalBytesSent;
+		ssize_t bytesSent = write(uartFd, remainingMessageToSend, bytesLeftToSend);
+		if (bytesSent < 0) {
+			Log_Debug("ERROR: Could not write to UART: %s (%d).\n", strerror(errno), errno);
+			terminationRequired = true;
+			return;
+		}
+
+		totalBytesSent += (size_t)bytesSent;
+	}
+
+	Log_Debug("Sent %zu bytes over UART in %d calls.\n", totalBytesSent, sendIterations);
+}
+
+/// <summary>
+///     Callback for curl_easy_perform() that copies all the downloaded chunks in a single memory
+///     block.
+/// <param name="chunks">The pointer to the chunks array</param>
+/// <param name="chunkSize">The size of each chunk</param>
+/// <param name="chunksCount">The count of the chunks</param>
+/// <param name="memoryBlock">The pointer where all the downloaded chunks are aggregated</param>
+/// </summary>
+static size_t StoreDownloadedDataCallback(void *chunks, size_t chunkSize, size_t chunksCount,
+                                          void *memoryBlock)
+{
+    MemoryBlock *block = (MemoryBlock *)memoryBlock;
+
+    size_t additionalDataSize = chunkSize * chunksCount;
+    block->data = realloc(block->data, block->size + additionalDataSize + 1);
+    if (block->data == NULL) {
+        Log_Debug("Out of memory, realloc returned NULL: errno=%d (%s)'n", errno, strerror(errno));
+        abort();
+    }
+
+    memcpy(block->data + block->size, chunks, additionalDataSize);
+    block->size += additionalDataSize;
+    block->data[block->size] = 0; // Ensure the block of memory is null terminated.
+
+    return additionalDataSize;
+}
+
+/// <summary>
+///     Logs a cURL error.
+/// </summary>
+/// <param name="message">The message to print</param>
+/// <param name="curlErrCode">The cURL error code to describe</param>
+static void LogCurlError(const char *message, int curlErrCode)
+{
+    Log_Debug(message);
+    Log_Debug(" (curl err=%d, '%s')\n", curlErrCode, curl_easy_strerror(curlErrCode));
+}
+
+/// <summary>
+///     Download a web page over HTTPS protocol using cURL.
+/// </summary>
+static void PerformWebPageDownload(void)
+{
+    CURL *curlHandle = NULL;
+    CURLcode res = 0;
+    MemoryBlock block = {.data = NULL, .size = 0};
+    char *certificatePath = NULL;
+
+    bool isNetworkingReady = false;
+    if ((Networking_IsNetworkingReady(&isNetworkingReady) < 0) || !isNetworkingReady) {
+        Log_Debug("\nNot doing download because network is not up.\n");
+		UART_LOG("\nNot doing download because network is not up.\n");
+        goto exitLabel;
+    }
+
+    
+	Log_Debug("\n -===- Starting download -===-\n");
+	UART_LOG("\n -===- Starting download -===-\n");
+
+    // Init the cURL library.
+    if ((res = curl_global_init(CURL_GLOBAL_ALL)) != CURLE_OK) {
+        LogCurlError("curl_global_init", res);
+        goto exitLabel;
+    }
+
+    if ((curlHandle = curl_easy_init()) == NULL) {
+        Log_Debug("curl_easy_init() failed\n");
+        goto cleanupLabel;
+    }
+
+    // Specify URL to download.
+    // Important: any change in the domain name must be reflected in the AllowedConnections
+    // capability in app_manifest.json.
+    if ((res = curl_easy_setopt(curlHandle, CURLOPT_URL, "https://www.baidu.com")) != CURLE_OK) {
+        LogCurlError("curl_easy_setopt CURLOPT_URL", res);
+        goto cleanupLabel;
+    }
+
+    // Set output level to verbose.
+    if ((res = curl_easy_setopt(curlHandle, CURLOPT_VERBOSE, 1L)) != CURLE_OK) {
+        LogCurlError("curl_easy_setopt CURLOPT_VERBOSE", res);
+        goto cleanupLabel;
+    }
+
+    // Get the full path to the certificate file used to authenticate the HTTPS server identity.
+    // The DigiCertGlobalRootCA.pem file is the certificate that is used to verify the
+    // server identity.
+    certificatePath = Storage_GetAbsolutePathInImagePackage("certs/DigiCertGlobalRootCA.pem");
+    if (certificatePath == NULL) {
+        Log_Debug("The certificate path could not be resolved: errno=%d (%s)\n", errno,
+                  strerror(errno));
+        goto cleanupLabel;
+    }
+
+    // Set the path for the certificate file that cURL uses to validate the server certificate.
+    if ((res = curl_easy_setopt(curlHandle, CURLOPT_CAINFO, certificatePath)) != CURLE_OK) {
+        LogCurlError("curl_easy_setopt CURLOPT_CAINFO", res);
+        goto cleanupLabel;
+    }
+
+    // Let cURL follow any HTTP 3xx redirects.
+    // Important: any redirection to different domain names requires that domain name to be added to
+    // app_manifest.json.
+    if ((res = curl_easy_setopt(curlHandle, CURLOPT_FOLLOWLOCATION, 1L)) != CURLE_OK) {
+        LogCurlError("curl_easy_setopt CURLOPT_FOLLOWLOCATION", res);
+        goto cleanupLabel;
+    }
+
+    // Set up callback for cURL to use when downloading data.
+    if ((res = curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, StoreDownloadedDataCallback)) !=
+        CURLE_OK) {
+        LogCurlError("curl_easy_setopt CURLOPT_FOLLOWLOCATION", res);
+        goto cleanupLabel;
+    }
+
+    // Set the custom parameter of the callback to the memory block.
+    if ((res = curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, (void *)&block)) != CURLE_OK) {
+        LogCurlError("curl_easy_setopt CURLOPT_WRITEDATA", res);
+        goto cleanupLabel;
+    }
+
+    // Specify a user agent.
+    if ((res = curl_easy_setopt(curlHandle, CURLOPT_USERAGENT, "libcurl-agent/1.0")) != CURLE_OK) {
+        LogCurlError("curl_easy_setopt CURLOPT_USERAGENT", res);
+        goto cleanupLabel;
+    }
+
+    // Perform the download of the web page.
+    if ((res = curl_easy_perform(curlHandle)) != CURLE_OK) {
+        LogCurlError("curl_easy_perform", res);
+    } else {		
+        Log_Debug("\n -===- Downloaded content (%zu bytes): -===-\n", block.size);
+        Log_Debug("%s\n", block.data);		
+		SendUartMessage(uartFd, block.data);
+		SendUartMessage(uartFd, "\n -===- Downloaded content (%zu bytes): -===-\n");
+    }
+
+cleanupLabel:
+    // Clean up allocated memory.
+    free(block.data);
+    free(certificatePath);
+    // Clean up sample's cURL resources.
+    curl_easy_cleanup(curlHandle);
+    // Clean up cURL library's resources.
+    curl_global_cleanup();
+    Log_Debug("\n -===- End of download -===-\n");
+	UART_LOG("\n -===- End of download -===-\n");
+
+exitLabel:
+    return;
+}
+
+/// <summary>
+///     The timer event handler.
+/// </summary>
+static void TimerEventHandler(EventData *eventData)
+{
+    if (ConsumeTimerFdEvent(webpageDownloadTimerFd) != 0) {
+        terminationRequired = true;
+        return;
+    }
+
+    PerformWebPageDownload();
+}
+
+// event handler data structures. Only the event handler field needs to be populated.
+static EventData timerEventData = {.eventHandler = &TimerEventHandler};
+
+/// <summary>
+///     Set up SIGTERM termination handler and event handlers.
+/// </summary>
+/// <returns>0 on success, or -1 on failure</returns>
+static int InitHandlers(void)
+{
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = TerminationHandler;
+    sigaction(SIGTERM, &action, NULL);
+
+    epollFd = CreateEpollFd();
+    if (epollFd < 0) {
+        return -1;
+    }
+
+    // Issue an HTTPS request at the specified period.
+    struct timespec tenSeconds = {10, 0};
+    webpageDownloadTimerFd =
+        CreateTimerFdAndAddToEpoll(epollFd, &tenSeconds, &timerEventData, EPOLLIN);
+    if (webpageDownloadTimerFd < 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+/// <summary>
+///     Clean up the resources previously allocated.
+/// </summary>
+static void CloseHandlers(void)
+{
+	CloseFdAndPrintError(uartFd, "Uart");
+    // Close the timer and epoll file descriptors.
+    CloseFdAndPrintError(webpageDownloadTimerFd, "WebpageDownloadTimer");
+    CloseFdAndPrintError(epollFd, "Epoll");
+}
+
+/// <summary>
+///     Main entry point for this sample.
+/// </summary>
+int main(int argc, char *argv[])
+{
+	Log_Debug("cURL easy interface based application starting.\n");
+	Log_Debug("This sample periodically attempts to download a webpage, using curl's 'easy' API.");
+	UART_LOG("cURL easy interface based application starting.\n");
+	UART_LOG("This sample periodically attempts to download a webpage, using curl's 'easy' API.");
+
+	gpioLedFd5 = GPIO_OpenAsOutput(MT3620_GPIO5, GPIO_OutputMode_PushPull, GPIO_Value_Low);
+	if (gpioLedFd5 < 0) {
+		Log_Debug("ERROR: Could not open LED GPIO: %s (%d).\n", strerror(errno), errno);
+		return -1;
+	}
+	gpioLedFd6 = GPIO_OpenAsOutput(MT3620_GPIO6, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	if (gpioLedFd6 < 0) {
+		Log_Debug("ERROR: Could not open LED GPIO: %s (%d).\n", strerror(errno), errno);
+		return -1;
+	}    
+
+	// Create a UART_Config object, open the UART and set up UART event handler
+	UART_Config uartConfig;
+	UART_InitConfig(&uartConfig);
+	uartConfig.baudRate = 115200;
+	uartConfig.flowControl = UART_FlowControl_None;
+	uartFd = UART_Open(MT3620_RDB_HEADER2_ISU0_UART, &uartConfig);
+	if (uartFd < 0) {
+		Log_Debug("ERROR: Could not open UART: %s (%d).\n", strerror(errno), errno);
+		return -1;
+	}
+
+    if (InitHandlers() != 0) {
+        terminationRequired = true;
+    } else {
+        // Download the web page immediately.
+        PerformWebPageDownload();
+    }
+
+    // Use epoll to wait for events and trigger handlers, until an error or SIGTERM happens
+    while (!terminationRequired) {
+        if (WaitForEventAndCallHandler(epollFd) != 0) {
+            terminationRequired = true;
+        }
+    }
+
+    CloseHandlers();
+    Log_Debug("Application exiting.\n");
+	UART_LOG("Application exiting.\n");
+    return 0;
+}
